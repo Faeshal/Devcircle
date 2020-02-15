209506a736bd39da8564e5ccdbaf7583405723ae
Faeshal <faeshal111@gmail.com>
2020-02-15 13:36:37 +0700

    setup mongodb atlas & connection + install colors console package

--- server.js
+++ server.js
@@ -2,18 +2,34 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
 const morgan = require("morgan");
+const colors = require("colors");
 const PORT = process.env.PORT || 5000;
 const bootcamps = require("./routes/bootcamps");
+const connectDB = require("./config/db");
 
 dotenv.config({ path: "./config/config.env" });
-app.use(morgan("short"));
+
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+// SECTION : Database Connection
+connectDB();
 
 // SECTION : Routing
 app.use("/api/v1/bootcamps", bootcamps);
 
 // SECTION : SERVER LISTEN
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(
-    `Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`
+    `Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`.green
+      .bold
   );
+});
+
+// Handle unhandled promise rejections
+process.on("unhandledRejection", (err, promise) => {
+  console.log("Error:" + err.message);
+  // Close server & exit process
+  server.close(() => process.exit(1));
 });
