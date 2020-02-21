feff046ba480e1d460968ede2e7084a633a43deb
Faeshal <faeshal111@gmail.com>
2020-02-21 09:22:56 +0700

    add update user details functionality

--- routes/auth.js
+++ routes/auth.js
@@ -5,7 +5,9 @@ const {
   login,
   getMe,
   forgotPassword,
-  resetPassword
+  resetPassword,
+  updateDetails,
+  updatePassword
 } = require("../controllers/auth");
 const { protect } = require("../middleware/auth");
 
@@ -14,5 +16,7 @@ router.post("/login", login);
 router.get("/me", protect, getMe);
 router.post("/forgotpassword", forgotPassword);
 router.put("/resetpassword/:resettoken", resetPassword);
+router.put("/updatedetails", protect, updateDetails);
+router.put("/updatepassword", protect, updatePassword);
 
 module.exports = router;
